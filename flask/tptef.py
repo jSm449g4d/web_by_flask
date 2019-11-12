6ab000e0e25a4c6dce8bbf342e72e00a33248bf9
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-11-13 05:13:24 +0900

    Add files via upload

--- flask/tptef.py
+++ flask/tptef.py
@@ -5,27 +5,28 @@ import sqlite3
 import hashlib
 import datetime
 
-def Display_Current_SQL(passwd=""):
+def Display_Current_SQL(room=""):
     html=""
     #sqlite3
     con=sqlite3.connect(os.path.join("./flask.sqlite"),isolation_level = None)
     cur=con.cursor()
-    cur.execute("create table if not exists nicoapi (sha256 text,url text,date datetime)")
-    sha256=hashlib.sha256(passwd.encode('utf-8')).hexdigest()
-    orders=cur.execute("select * from nicoapi where sha256=\"%s\""%sha256).fetchall()
+    cur.execute("create table if not exists tptef (room text,user text,remark text,sha256 text,date datetime)")
+    orders=cur.execute("select * from tptef where room=\"%s\""%room).fetchall()
     cur.close();con.close()
     #add_texts_into_html
     for order in orders:
         html+="<tr><td class=\"flask_table\">"+str(order[1])+"</td>"
-        html+="<td class=\"flask_table\">"+str(order[2])+"</td></tr>"
+        html+="<td class=\"flask_table\">"+str(order[2])+"</td>"
+        html+="<td class=\"flask_table\" style:\"\">"+str(order[3])+"</td>"
+        html+="<td class=\"flask_table\">"+str(order[4])+"</td></tr>"
     return html
 
-def Order_Into_SQL(passwd="",order=""):
+def Order_Into_SQL(room="",user="",remark="",passwd=""):
     #sqlite3
     con=sqlite3.connect(os.path.join("./flask.sqlite"),isolation_level = None)
     cur=con.cursor()
     sha256=hashlib.sha256(passwd.encode('utf-8')).hexdigest()
-    cur.execute("insert into nicoapi values(?,?,?)",[sha256,order,datetime.datetime.now()])
+    cur.execute("insert into tptef values(?,?,?,?,?)",[room,user,remark,sha256,datetime.datetime.now()])
     cur.close();con.close()
 
 def Clear_Order_SQL(passwd=""):
@@ -33,7 +34,7 @@ def Clear_Order_SQL(passwd=""):
     con=sqlite3.connect(os.path.join("./flask.sqlite"),isolation_level = None)
     cur=con.cursor()
     sha256=hashlib.sha256(passwd.encode('utf-8')).hexdigest()
-    cur.execute("delete from nicoapi where sha256=\"%s\""%sha256)
+    cur.execute("delete from tptef where sha256=\"%s\""%sha256)
     cur.close();con.close()
 
 def render_template_2(dir,**kwargs):
@@ -46,17 +47,23 @@ def render_template_2(dir,**kwargs):
 
 def show(req):
     os.chdir(os.path.dirname(__file__))
+    room=""
+    user=""
+    remark=""
     passwd=""
-    urls="https://api.search.nicovideo.jp/api/v2/video/contents/search"
     if req.method == 'POST':
-        if 'url' in req.form:#set_url
-            urls=req.form['url']
-        if 'pass' in req.form:#set_username
+        if 'room' in req.form:
+            room=req.form['room']
+        if 'user' in req.form:
+            user=req.form['user']
+        if 'remark' in req.form:
+            remark=req.form['remark']
+        if 'pass' in req.form:
             passwd=secure_filename(req.form['pass'])
         if "launch" in req.form and secure_filename(req.form["launch"])=="True":
-            Order_Into_SQL(passwd,urls)
+            Order_Into_SQL(room,user,remark,passwd)
         if "clear" in req.form and secure_filename(req.form["clear"])=="True":
             Clear_Order_SQL(passwd)
 
-    orders=Display_Current_SQL(passwd)
-    return render_template_2("nicoapi.html",ORDERS=orders,URL=urls,PASS=passwd)
+    orders=Display_Current_SQL(room)
+    return render_template_2("tptef.html",ORDERS=orders,ROOM=room,USER=user,REMARK=remark,PASS=passwd)
