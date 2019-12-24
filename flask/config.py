f7e9b8f538f8015ccdc00d7599418764a76e0edc
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-25 00:04:24 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -9,15 +9,24 @@ import os
 from google.cloud import storage
 from datetime import datetime
 import pytz
+import json
 
 status_GCS="error"
-dir_db='./flask.sqlite3'
-dir_gcp_key="FirebaseAdminKey.json"
-GCS_bucket="fb_gcs_bucket"
-GCS_blob='flask.sqlite3'
+dir_config_json='./config.json'
+config_dict={"dir_db":'./flask.sqlite3',"dir_gcp_key":"FirebaseAdminKey.json",
+            "GCS_bucket":"fb_gcs_bucket","GCS_blob":"flask.sqlite3"}
+
+if os.path.exists(dir_config_json):
+    with open(dir_config_json,"r",encoding="utf-8") as fp:
+        config_dict=json.load(fp)    
+else:
+    with open(dir_config_json,"w",encoding="utf-8") as fp:
+        json.dump(config_dict,fp)
+    os.chmod(dir_config_json,0o777)
+
 
 try:#get_key
-    storage_client = storage.Client.from_service_account_json(dir_gcp_key)
+    storage_client = storage.Client.from_service_account_json(config_dict["dir_gcp_key"])
     status_GCS="access success"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
 except:
     status_GCS="not_available"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
@@ -35,21 +44,22 @@ def show(req):
     if req.method == 'POST':
         if "gcs_upload" in req.form and secure_filename(req.form["gcs_upload"])=="True":
             try:
-                storage_client.get_bucket(GCS_bucket).blob(GCS_blob).upload_from_filename(dir_db)
+                storage_client.get_bucket(config_dict["GCS_bucket"]).blob(config_dict["GCS_blob"]).upload_from_filename(config_dict["dir_db"])
                 status_GCS="APP→GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
             except:
                 status_GCS="error: APP→×GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
         if "gcs_download" in req.form and secure_filename(req.form["gcs_download"])=="True":
             try:
-                storage_client.get_bucket(GCS_bucket).blob(GCS_blob).download_to_filename(dir_db)
+                storage_client.get_bucket(config_dict["GCS_bucket"]).blob(config_dict["GCS_blob"]).download_to_filename(config_dict["dir_db"])
                 status_GCS="GCS→APP"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
             except:
                 status_GCS="error: GCS→×APP"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
         if "gcs_client_reload" in req.form and secure_filename(req.form["gcs_client_reload"])=="True":
             try:#get_key
-                storage_client = storage.Client.from_service_account_json(dir_gcp_key)
+                storage_client = storage.Client.from_service_account_json(config_dict["dir_gcp_key"])
                 status_GCS="success gcs_client_reload"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
             except:
                 status_GCS="error: ×gcs_client_reload"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
-    return render_template_2("config.html",STATUS_GCS=status_GCS,DIR_DB=dir_db,GCS_BUCKET=GCS_bucket,GCS_BLOB=GCS_blob)
+    return render_template_2("config.html",STATUS_GCS=status_GCS,DIR_DB=config_dict["dir_db"],GCS_BUCKET=config_dict["GCS_bucket"],
+                            GCS_BLOB=config_dict["GCS_blob"])
 
