d9c9eb3db7aefb43c6294e5b81c73a131fe27271
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-25 00:19:50 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -13,15 +13,13 @@ import json
 
 status_GCS="error"
 dir_config_json='./config.json'
-config_dict={"dir_db":'./flask.sqlite3',"dir_gcp_key":"FirebaseAdminKey.json",
+config_dict={"dir_db":'flask.sqlite3',"dir_gcp_key":"FirebaseAdminKey.json",
             "GCS_bucket":"fb_gcs_bucket","GCS_blob":"flask.sqlite3"}
 
 if os.path.exists(dir_config_json):
-    with open(dir_config_json,"r",encoding="utf-8") as fp:
-        config_dict=json.load(fp)    
+    with open(dir_config_json,"r",encoding="utf-8") as fp:config_dict=json.load(fp)    
 else:
-    with open(dir_config_json,"w",encoding="utf-8") as fp:
-        json.dump(config_dict,fp)
+    with open(dir_config_json,"w",encoding="utf-8") as fp:json.dump(config_dict,fp)
     os.chmod(dir_config_json,0o777)
 
 
@@ -38,6 +36,19 @@ def render_template_2(dir,**kwargs):
         for kw,arg in kwargs.items():
             html=html.replace("{{"+kw+"}}",arg)
     return render_template_string(html)
+
+def config_json_update(form={}):
+    global config_dict
+    if "status_GCS" in form:
+        config_dict["status_GCS"]=secure_filename(form["status_GCS"])
+    if "GCS_bucket" in form:
+        config_dict["GCS_bucket"]=secure_filename(form["GCS_bucket"])
+    if "GCS_blob" in form:
+        config_dict["GCS_blob"]=secure_filename(form["GCS_blob"])
+    if "dir_gcp_key" in form:
+        config_dict["dir_gcp_key"]=secure_filename(form["dir_gcp_key"])
+    with open(dir_config_json,"w+",encoding="utf-8") as fp:json.dump(config_dict,fp)
+    os.chmod(dir_config_json,0o777)
 
 def show(req):
     global status_GCS;global storage_client
@@ -60,6 +71,7 @@ def show(req):
                 status_GCS="success gcs_client_reload"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
             except:
                 status_GCS="error: ×gcs_client_reload"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
+        config_json_update(req.form)
     return render_template_2("config.html",STATUS_GCS=status_GCS,DIR_DB=config_dict["dir_db"],GCS_BUCKET=config_dict["GCS_bucket"],
-                            GCS_BLOB=config_dict["GCS_blob"])
+                            GCS_BLOB=config_dict["GCS_blob"],DIR_GCP_KEY=config_dict["dir_gcp_key"])
 
