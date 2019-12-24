868b6e9399ffe7afaa3b587a12aa5f98af29a048
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-24 22:08:56 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -10,14 +10,14 @@ from google.cloud import storage
 from datetime import datetime
 import pytz
 
-config_status_gcs="not_available"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
+status="not_available"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
 dir_db='./flask.sqlite3'
 GCS_bucket="fb_gcs_bucket"
 GCS_blob='flask.sqlite3'
 
 try:#get_key
     storage_clienta = storage.Client.from_service_account_json("FirebaseAdminKey.json")
-    config_status_gcs="access success"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
+    status="access success"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
 except:0
 
 def render_template_2(dir,**kwargs):
@@ -29,13 +29,14 @@ def render_template_2(dir,**kwargs):
     return render_template_string(html)
 
 def show(req):
+    status_GCS=status
     if req.method == 'POST':
         if "gcs_upload" in req.form and secure_filename(req.form["gcs_upload"])=="True":
             try:
                 bucket= storage_clienta.get_bucket(GCS_bucket)
                 bucket.blob(GCS_blob).upload_from_filename(dir_db)
-                config_status_gcs="APP→GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
+                status_GCS="APP→GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
             except:
-                config_status_gcs="error: APP→×GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
-    return render_template_2("config.html",STATUS_GCS=config_status_gcs,DIR_DB=dir_db,GCS_BUCKET=GCS_bucket,GCS_BLOB=GCS_blob)
+                status_GCS="error: APP→×GCS"+datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")
+    return render_template_2("config.html",STATUS_GCS=status_GCS,DIR_DB=dir_db,GCS_BUCKET=GCS_bucket,GCS_BLOB=GCS_blob)
 
