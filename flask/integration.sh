8c65ae6c5bc36e69172065cd99f592d8f4543044
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-26 23:22:29 +0900

    Add files via upload

--- flask/integration.sh
+++ flask/integration.sh
@@ -1,5 +1,5 @@
 #Integration test
 waitress-serve --host=127.0.0.1 --port=8080 wsgi:app &
-result_integration=python integration.py
+result_integration=bash -c "python integration.py"
 pkill -f waitress-serve
 exit $result_integration
