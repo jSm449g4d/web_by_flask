fd5b305526482adbbdeb121fa0e616cca02d391b
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-27 04:13:18 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -20,7 +20,6 @@ config_dict={"dir_db":'flask.sqlite3',"dir_gcp_key":"FirebaseAdminKey.json",
             "GCS_bucket":"fb_gcs_bucket","GCS_blob":"flask.sqlite3",
             "FB_admin_uid":"1GYEMV6s2OWU9dR2cXCntSlR2op2"}
 iii=0
-fbtoken="-"
 
 if os.path.exists(dir_config_json):
     with open(dir_config_json,"r",encoding="utf-8") as fp:config_dict=json.load(fp)    
@@ -65,9 +64,9 @@ def html_create_recode(title="",data=""):
     
 
 def show(req):
-    global status_GCS,storage_client,fbtoken,config_dict;
+    global status_GCS,storage_client,config_dict;
     global iii;iii+=1
-    status_table=""
+    status_table="";fbtoken="";
     if req.method == 'POST':
         if "fbtoken" in req.form:#firebase_token front→backend
             fbtoken=secure_filename(req.form["fbtoken"])
