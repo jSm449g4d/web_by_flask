633e09a0d999ad0989446b683840c915832aae4c
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2019-12-27 19:35:33 +0900

    Add files via upload

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -36,12 +36,16 @@ except:print("cant unzip CDN contents")
 
 @app.route("/")
 def indexpage_show():
+    fbtoken=""
+    if request.method == 'POST':
+        if "fbtoken" in request.form:fbtoken=secure_filename(request.form["fbtoken"])
     global access_counter;access_counter+=1
     return render_template("index.html",
     used_python=sys.version,
     used_flask=flask.__version__,
     used_sqlite3=sqlite3.version,
     access_counter=str(access_counter),
+    FBTOKEN=fbtoken
     )
 
 @app.route("/<name>.html")
