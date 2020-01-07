b28d32cb68c93273ea99dc1a50a73454688560ec
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-01-07 15:34:21 +0900

    Add files via upload

--- flask/integration.sh
+++ flask/integration.sh
@@ -1,6 +1,6 @@
 #Integration test 1/2
 waitress-serve --host=127.0.0.1 --port=8080 wsgi:app &
-result_integration=$(python integration.py)
+result_integration=$(python3 integration.py)
 pkill -f waitress-serve
 if [ $result_integration = "ok" ]; then
     echo PASS:Integration Test;exit 0
