deddc3f8d8092b416da40b0517450b887a4667d6
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-01-11 20:10:23 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -120,8 +120,10 @@ def show(req):
                         autocommit=True)
                     status_table+=html_create_recode("MySQL","OK")
                     conn.close()
-                except as e:
-                    status_table+=html_create_recode("MySQL_err",e)
+                except Exception as e:
+                    status_table+=html_create_recode("MySQL_err","a")
+                except:
+                    status_table+=html_create_recode("MySQL","ERROR")
         #/Operation
     return wsgi.render_template_2("config.html",STATUS_GCS=status_GCS,DIR_DB=config_dict["dir_db"],
                             form_gcs_uri=config_dict["form_gcs_uri"],DIR_GCP_KEY=config_dict["dir_gcp_key"],
