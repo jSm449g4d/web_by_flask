e8443bce8beb69403bc446fbbfd737324beb76bf
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-02-05 15:42:05 +0900

    Add files via upload

--- flask/config.py
+++ flask/config.py
@@ -129,7 +129,9 @@ def show(req):
                     Base.metadata.create_all(dbengine)
                     Session.add(
                     testtable(id=iii,date =datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)"),temperature =0))
-                    Session.add(tsimple(id=1))
+                    te=tsimple()
+                    te.id=100
+                    Session.add(te)
                     dbengine.dispose()
                     status_table+=html_create_recode("sqlalchemy","Ced")
                 except Exception as e:
