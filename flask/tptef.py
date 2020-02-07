a8ddbd7b1b342993f9ee676fb0d58917f071f171
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-02-07 16:26:23 +0900

    Add files via upload

--- flask/tptef.py
+++ flask/tptef.py
@@ -27,8 +27,8 @@ def Display_Current_SQL(room=""):
     try:
         session = sessionmaker(bind=wsgi.dbengine)()
         Base.metadata.create_all(wsgi.dbengine)
-        session.add(tptef_table(room="sqlarchemytst",user ="a",remark="tst",sha256="tst"
-                            ,date = datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")))
+#        session.add(tptef_table(room="sqlarchemytst",user ="a",remark="tst",sha256="tst"
+#                            ,date = datetime.now(pytz.UTC).strftime(" %Y/%m/%d %H:%M:%S (UTC)")))
         #aaa=session.query(tptef_table).filter(tptef_table.room == room)
         session.commit()
         session.close()
