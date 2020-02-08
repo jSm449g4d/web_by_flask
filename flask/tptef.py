9fcbdd68050f926c2c594f99ce4de6da798a5efa
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-02-09 00:33:55 +0900

    Add files via upload

--- flask/tptef.py
+++ flask/tptef.py
@@ -27,6 +27,7 @@ def show(req):
     content=""
     passwd=""
     fbtoken=""
+    orders=""
     session = sessionmaker(bind=wsgi_util.dbengine)()
     Base.metadata.create_all(wsgi_util.dbengine)
     if req.method == 'POST':
@@ -45,7 +46,6 @@ def show(req):
                             date = datetime.now(pytz.UTC).strftime("%Y/%m/%d %H:%M:%S %f (UTC)")))
         if "clear" in req.form and secure_filename(req.form["clear"])=="True":
             session.query(table).filter(table.trip == hashlib.sha256(passwd.encode('utf-8')).hexdigest(),table.room==room).delete()
-
     #show chat thread
     for order in session.query(table).filter(table.room == room):
         orders+="<tr><td>"+order.user+"</td>"
@@ -53,7 +53,7 @@ def show(req):
         orders+="<td style=\"font-size: 12px;\">"+order.trip[:16]+"<br>"+order.trip[16:32]+\
         "<br>"+order.trip[32:48]+"<br>"+order.trip[48:64]+"</td>"
         orders+="<td style=\"font-size: 12px;\">"+order.date+"</td></tr>"
-    return "a"
+    
     session.commit()
     session.close()
     
