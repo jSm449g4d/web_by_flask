265b38dafe46ef377a5bb4d7b2709d6f66d87df1
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-02-08 23:08:45 +0900

    Add files via upload

--- flask/tptef.py
+++ flask/tptef.py
@@ -11,50 +11,51 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import wsgi_util
 
-Base = declarative_base()
-class table(Base):
-    __tablename__ = 'tptef'
-    room=Column(String(32))
-    user=Column(String(32))
-    content=Column(String(256))
-    trip=Column(String(64))
-    date = Column(String(64),primary_key= True)
+#Base = declarative_base()
+#class table(Base):
+#    __tablename__ = 'tptef'
+#    room=Column(String(32))
+#    user=Column(String(32))
+#    content=Column(String(256))
+#    trip=Column(String(64))
+#    date = Column(String(64),primary_key= True)
 
 def show(req):
-    print("AAAA")
-    room=""
-    user=""
-    content=""
-    passwd=""
-    fbtoken=""
-    orders=str(wsgi_util.dbengine)[:25]+"||"+str(wsgi_util.access_counter)
-    session = sessionmaker(bind=wsgi_util.dbengine)()
-    Base.metadata.create_all(wsgi_util.dbengine)
-    if req.method == 'POST':
-        if "fbtoken" in req.form:fbtoken=secure_filename(req.form["fbtoken"])#Firebase_Token_keep
-        if 'room' in req.form:
-            room=req.form['room'].translate(str.maketrans("","","\"\'\\/<>%`?;"))#Not_secure_filename!
-        if 'user' in req.form:
-            user=req.form['user'].translate(str.maketrans("","","\"\'\\/<>%`?;"))#Not_secure_filename!
-        if 'content' in req.form:
-            content=req.form['content'].translate(str.maketrans("\"\'\\/<>%`?;",'””￥_〈〉％”？；'))#Not_secure_filename!
-        if 'pass' in req.form:
-            passwd=secure_filename(req.form['pass'])
-        if "remark" in req.form and secure_filename(req.form["remark"])=="True":
-            session.add(table(room=room,user =user,content=content,
-                            trip=hashlib.sha256(passwd.encode('utf-8')).hexdigest(),
-                            date = datetime.now(pytz.UTC).strftime("%Y/%m/%d %H:%M:%S %f (UTC)")))
-        if "clear" in req.form and secure_filename(req.form["clear"])=="True":
-            session.query(table).filter(table.trip == hashlib.sha256(passwd.encode('utf-8')).hexdigest(),table.room==room).delete()
+    return "a"
+    
+#    print("AAAA")
+#    room=""
+#    user=""
+#    content=""
+#    passwd=""
+#    fbtoken=""
+#    session = sessionmaker(bind=wsgi_util.dbengine)()
+#    Base.metadata.create_all(wsgi_util.dbengine)
+#    if req.method == 'POST':
+#        if "fbtoken" in req.form:fbtoken=secure_filename(req.form["fbtoken"])#Firebase_Token_keep
+#        if 'room' in req.form:
+#            room=req.form['room'].translate(str.maketrans("","","\"\'\\/<>%`?;"))#Not_secure_filename!
+#        if 'user' in req.form:
+#            user=req.form['user'].translate(str.maketrans("","","\"\'\\/<>%`?;"))#Not_secure_filename!
+#        if 'content' in req.form:
+#            content=req.form['content'].translate(str.maketrans("\"\'\\/<>%`?;",'””￥_〈〉％”？；'))#Not_secure_filename!
+#        if 'pass' in req.form:
+#            passwd=secure_filename(req.form['pass'])
+#        if "remark" in req.form and secure_filename(req.form["remark"])=="True":
+#            session.add(table(room=room,user =user,content=content,
+#                            trip=hashlib.sha256(passwd.encode('utf-8')).hexdigest(),
+#                            date = datetime.now(pytz.UTC).strftime("%Y/%m/%d %H:%M:%S %f (UTC)")))
+#        if "clear" in req.form and secure_filename(req.form["clear"])=="True":
+#            session.query(table).filter(table.trip == hashlib.sha256(passwd.encode('utf-8')).hexdigest(),table.room==room).delete()
 
     #show chat thread
-    for order in session.query(table).filter(table.room == room):
-        orders+="<tr><td>"+order.user+"</td>"
-        orders+="<td>"+order.content+"</td>"
-        orders+="<td style=\"font-size: 12px;\">"+order.trip[:16]+"<br>"+order.trip[16:32]+\
-        "<br>"+order.trip[32:48]+"<br>"+order.trip[48:64]+"</td>"
-        orders+="<td style=\"font-size: 12px;\">"+order.date+"</td></tr>"
-    session.commit()
-    session.close()
+#    for order in session.query(table).filter(table.room == room):
+#        orders+="<tr><td>"+order.user+"</td>"
+#        orders+="<td>"+order.content+"</td>"
+#        orders+="<td style=\"font-size: 12px;\">"+order.trip[:16]+"<br>"+order.trip[16:32]+\
+#        "<br>"+order.trip[32:48]+"<br>"+order.trip[48:64]+"</td>"
+#        orders+="<td style=\"font-size: 12px;\">"+order.date+"</td></tr>"
+#    session.commit()
+#    session.close()
     
     return wsgi_util.render_template_2("tptef.html",ORDERS=orders,ROOM=room,USER=user,PASS=passwd)
